c81327424d2877dd21e254c356b6ce51365797fa
Roberto Evangelista <robertoevangelistasf@gmail.com>
2024-02-09 15:37:28 +0100

    feat: remove redundant SignUp factory and move it to SignUpComposer

--- Main/Composers/SignUpComposer.swift
+++ Main/Composers/SignUpComposer.swift
@@ -1,9 +1,19 @@
 import Foundation
 import UIMobile
+import Presentation
+import Validation
 import Domain
 
 public final class SignUpComposer {
     public static func composeController(with addAccount: AddAccount) -> SignUpViewController {
-        return ControllerFactory.makeSignUp(addAccount: addAccount)
+        let controller = SignUpViewController.instantiate()
+        let presenter = SignUpPresenter(
+            alertView: WeakVarProxy(controller),
+            emailValidator: EmailValidatorAdapter(),
+            addAccount: addAccount,
+            loadingView: WeakVarProxy(controller)
+        )
+        controller.signUp = presenter.signUp
+        return controller
     }
 }
