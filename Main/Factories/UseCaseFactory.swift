3f3756f5c719d409b44a359309c894a6496b341b
Roberto Evangelista <robertoevangelistasf@gmail.com>
2024-02-09 16:01:36 +0100

    refactor: improve code readability

--- Main/Factories/UseCaseFactory.swift
+++ Main/Factories/UseCaseFactory.swift
@@ -4,9 +4,14 @@ import Infra
 import Domain
 
 final class UseCaseFactory {
+    private static let httpClient: HttpPostClient = AlamofireAdapter()
+    private static let baseUrl = "https://clean-note-api.herokuapp.com/api"
+    
+    private static func makeUrl(path: String) -> URL {
+        return URL(string: "\(baseUrl)/\(path)")!
+    }
+    
     static func makeRemoteAddAccount() -> AddAccount {
-        let url = URL(string: "https://clean-note-api.herokuapp.com/api/signup")!
-        let alamofireAdapter = AlamofireAdapter()
-        return RemoteAddAccount(url: url, httpClient: alamofireAdapter)
+        return RemoteAddAccount(url: makeUrl(path: "signup"), httpClient: httpClient)
     }
 }
