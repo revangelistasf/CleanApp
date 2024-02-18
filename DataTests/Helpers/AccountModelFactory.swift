ea1c05614e81d053c4118793cd1816d0983af766
Roberto Evangelista <robertoevangelistasf@gmail.com>
2024-02-18 21:37:24 +0100

    update: Account model to new access token property

--- DataTests/Helpers/AccountModelFactory.swift
+++ DataTests/Helpers/AccountModelFactory.swift
@@ -2,12 +2,7 @@ import Foundation
 import Domain
 
 func makeAccountModel() -> AccountModel {
-    AccountModel(
-        id: "any_id",
-        name: "any_name",
-        email: "any_email@mail.com",
-        password: "any_password"
-    )
+    AccountModel(accessToken: "any_token")
 }
 
 func makeAddAccountModel() -> AddAccountModel {
