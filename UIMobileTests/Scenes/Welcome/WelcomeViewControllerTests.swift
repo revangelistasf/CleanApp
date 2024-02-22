6ace0238947bcc9f7cb859e6da9ffd333f63280e
Roberto Evangelista <robertoevangelistasf@gmail.com>
2024-02-22 10:03:17 +0100

    feat: add welcome storyboard

---
+++ UIMobileTests/Scenes/Welcome/WelcomeViewControllerTests.swift
@@ -0,0 +1,39 @@
+import XCTest
+import UIKit
+import Presentation
+@testable import UIMobile
+
+final class WelcomeViewControllerTests: XCTestCase {
+    func test_saveButton_callsSignUpOnTap() {
+        var signUpViewModel: SignUpViewModel?
+        let sut = makeSut(signUpViewModel: { signUpViewModel = $0 })
+        sut.saveButton?.simulateTap()
+        let name = sut.nameTextField?.text
+        let email = sut.emailTextField.text
+        let password = sut.passwordTextField.text
+        let passwordConfirmation = sut.passwordConfirmationTextField.text
+        XCTAssertEqual(
+            signUpViewModel,
+            SignUpViewModel(
+                name: name,
+                email: email,
+                password: password,
+                passwordConfirmation: passwordConfirmation
+            )
+        )
+    }
+}
+
+extension WelcomeViewControllerTests {
+    private func makeSut(
+        signUpViewModel: ((SignUpViewModel) -> Void)? = nil,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> SignUpViewController {
+        let sut = SignUpViewController.instantiate()
+        sut.signUp = signUpViewModel
+        sut.loadViewIfNeeded()
+        checkMemoryLeak(for: sut, file: file, line: line)
+        return sut
+    }
+}
