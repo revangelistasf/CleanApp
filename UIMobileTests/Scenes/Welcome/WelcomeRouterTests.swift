80302ca766dec22634be12e8d2eb3cc739882a29
Roberto Evangelista <robertoevangelistasf@gmail.com>
2024-02-22 17:13:46 +0100

    feat: add WelcomeRouter goToSignUp

--- UIMobileTests/Scenes/Welcome/WelcomeRouterTests.swift
+++ UIMobileTests/Scenes/Welcome/WelcomeRouterTests.swift
@@ -4,39 +4,54 @@ import UIKit
 
 final class WelcomeRouter {
     private let navigationController: NavigationController
-    private let loginFactory: () -> LoginViewController
+    private let loginControllerFactory: () -> LoginViewController
+    private let signUpControllerFactory: () -> SignUpViewController
     
     public init(
         navigationController: NavigationController,
-        loginFactory: @escaping () -> LoginViewController
+        loginControllerFactory: @escaping () -> LoginViewController,
+        signUpControllerFactory: @escaping () -> SignUpViewController
     ) {
         self.navigationController = navigationController
-        self.loginFactory = loginFactory
+        self.loginControllerFactory = loginControllerFactory
+        self.signUpControllerFactory = signUpControllerFactory
     }
     
-    func goToLogin() {
-        navigationController.pushViewController(loginFactory())
+    public func goToLogin() {
+        navigationController.pushViewController(loginControllerFactory())
+    }
+    
+    public func goToSignUp() {
+        navigationController.pushViewController(signUpControllerFactory())
     }
 }
 
 final class WelcomeRouterTests: XCTestCase {
-    func test_login_goToLogin_navigationControllerPresentLoginViewController() {
+    func test_goToLogin_navigationControllerPushLoginViewController() {
         let navigationController = NavigationController()
         makeSut(navigationController: navigationController).goToLogin()
         XCTAssertEqual(navigationController.viewControllers.count, 1)
         XCTAssertTrue(navigationController.viewControllers.first is LoginViewController)
     }
-
+    
+    func test_goToSignUp_navigationControllerPushSignUpViewController() {
+        let navigationController = NavigationController()
+        makeSut(navigationController: navigationController).goToSignUp()
+        XCTAssertEqual(navigationController.viewControllers.count, 1)
+        XCTAssertTrue(navigationController.viewControllers.first is SignUpViewController)
+    }
 }
 
 extension WelcomeRouterTests {
     func makeSut(
         navigationController: NavigationController = NavigationController()
     ) -> WelcomeRouter {
-        let loginFactorySpy = LoginFactorySpy()
+        let loginControllerFactorySpy = LoginControllerFactorySpy()
+        let signUpControllerFactorySpy = SignUpControllerFactorySpy()
         let sut = WelcomeRouter(
             navigationController: navigationController,
-            loginFactory: loginFactorySpy.makeLogin
+            loginControllerFactory: loginControllerFactorySpy.makeLogin,
+            signUpControllerFactory: signUpControllerFactorySpy.makeSignUp
         )
         checkMemoryLeak(for: sut)
         return sut
@@ -44,9 +59,15 @@ extension WelcomeRouterTests {
 }
 
 extension WelcomeRouterTests {
-    class LoginFactorySpy {
+    class LoginControllerFactorySpy {
         func makeLogin() -> LoginViewController {
             return LoginViewController.instantiate()
+        }
+    }    
+    
+    class SignUpControllerFactorySpy {
+        func makeSignUp() -> SignUpViewController {
+            return SignUpViewController.instantiate()
         }
     }
 }
